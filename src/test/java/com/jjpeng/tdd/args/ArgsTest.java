3fb1999782ba993221e87ed6b4385c2a78a47701
Jiajun-Peng <pengjiajun566@gmail.com>
2023-04-05 17:13:10 +0800

    将最终要实现的功能转化为两个测试

--- src/test/java/com/jjpeng/tdd/args/ArgsTest.java
+++ src/test/java/com/jjpeng/tdd/args/ArgsTest.java
@@ -13,13 +13,23 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ArgsTest {
 
     @Test
-    public void should() {
+    public void should_example_1() {
         Options options = Args.parse(Options.class, "-l", "-p", "8080", "-d", "/usr/logs");
-        options.logging();
-        options.port();
-        options.directory();
+        assertTrue(options.logging());
+        assertEquals(8080, options.port());
+        assertEquals("/usr/logs", options.directory());
+    }
+
+    @Test
+    public void should_example_2() {
+        ListOptions options = Args.parse(ListOptions.class, "-g", "this", "is", "a", "list", "-d", "1", "2", "3", "-5");
+        assertArrayEquals(new String[]{"this", "is", "a", "list"}, options.group());
+        assertArrayEquals(new int[]{1, 2, 3, -5}, options.decimals());
     }
 
     static record Options(@Option("l")boolean logging, @Option("p")int port, @Option("d")String directory) {
+    }
+
+    static record ListOptions(@Option("g")String[] group, @Option("d")int[] decimals) {
     }
 }

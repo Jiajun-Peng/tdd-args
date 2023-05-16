b96e15bbe8bbd9d3a21212927134b34d1315d000
Jiajun-Peng <pengjiajun566@gmail.com>
2023-05-17 07:28:22 +0800

    重构，将IntegerParser和StringParser的公共部分提取为相同的代码

--- src/main/java/com/jjpeng/tdd/args/IntegerParser.java
+++ src/main/java/com/jjpeng/tdd/args/IntegerParser.java
@@ -8,6 +8,10 @@ class IntegerParser implements OptionParser {
     public Object parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
         String value = arguments.get(index + 1);
+        return parseValue(value);
+    }
+
+    private static int parseValue(String value) {
         return Integer.parseInt(value);
     }
 }

4c1ddc8c9a8b075a7e6d851f709d558077d356c2
Jiajun-Peng <pengjiajun566@gmail.com>
2023-05-17 08:13:18 +0800

    重构，将IntegerParser执行的parseValue传递给父类执行

--- src/main/java/com/jjpeng/tdd/args/IntegerParser.java
+++ src/main/java/com/jjpeng/tdd/args/IntegerParser.java
@@ -2,8 +2,7 @@ package com.jjpeng.tdd.args;
 
 class IntegerParser extends StringParser {
 
-    @Override
-    protected Object parseValue(String value) {
-        return Integer.parseInt(value);
+    public IntegerParser() {
+        super(Integer::parseInt);
     }
 }

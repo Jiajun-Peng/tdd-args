38aadbea87be8ad409d5129266cb2b778b6c9864
Jiajun-Peng <pengjiajun566@gmail.com>
2023-05-23 07:53:54 +0800

    构造对-p 8080 8081的测试

--- src/test/java/com/jjpeng/tdd/args/SingleValueOptionParserTest.java
+++ src/test/java/com/jjpeng/tdd/args/SingleValueOptionParserTest.java
@@ -1,8 +1,22 @@
 package com.jjpeng.tdd.args;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static com.jjpeng.tdd.args.BooleanOptionParserTest.option;
+
 public class SingleValueOptionParserTest {
     // Sad path:
     // TODO: - Integer -p 8080 8081
+    @Test
+    public void should_not_accept_extra_argument() {
+        Assertions.assertThrows(TooManyArgumentsException.class, () -> {
+            new SingleValueOptionParser<>(Integer::parseInt).parse(Arrays.asList("-p", "8080", "8081"), option("p"));
+        });
+    }
+
     // TODO: - String -d / -d /usr/logs /usr/vars
 
     // default value:

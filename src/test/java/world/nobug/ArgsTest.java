823628b4fcdfd88b93f5194a178954b24a5dc718
Jiajun-Peng <pengjiajun566@gmail.com>
2024-06-29 07:26:38 +0800

    先构思接口大致可能被使用的方式，列举出两个示例，不管编译是否能通过。

---
+++ src/test/java/world/nobug/ArgsTest.java
@@ -0,0 +1,34 @@
+package world.nobug;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class ArgsTest {
+
+    // -l -p 8080 -d /usr/logs
+    @Test
+    public void should_example_1() {
+        Options options = Args.parse(Options.class, "-l", "-p", "8080", "-d", "/usr/logs");
+        assertTrue(options.logging());
+        assertEquals(8080, options.port());
+        assertEquals("/usr/logs", options.directory());
+    }
+
+    // -g this is a list -d 1 2 -3 5
+    @Test
+    public void should_example_2() {
+        ListOptions options = Args.parse(ListOptions.class, "-g", "this", "is", "a", "list", "-d", "1", "2", "-3", "5");
+        assertArrayEquals(new String[] { "this", "is", "a", "list" }, options.group());
+        assertArrayEquals(new int[] { 1, 2, -3, 5}, options.decimals());
+    }
+
+    static record Options(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
+
+    }
+
+    static record ListOptions(@Option("g") String[] group, @Option("d") int[] decimals) {
+
+    }
+}

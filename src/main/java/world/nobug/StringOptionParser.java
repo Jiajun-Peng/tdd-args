faa1327361abc79f2cde00091a18572aef416271
Jiajun-Peng <pengjiajun566@gmail.com>
2024-06-30 23:01:36 +0800

    重构，移除StringOptionParser的构造函数

--- src/main/java/world/nobug/StringOptionParser.java
+++ src/main/java/world/nobug/StringOptionParser.java
@@ -2,11 +2,7 @@ package world.nobug;
 
 class StringOptionParser extends IntOptionParser {
 
-    private StringOptionParser() {
-        super(String::valueOf);
-    }
-
     public static OptionParser createStringOptionParser() {
-        return new StringOptionParser();
+        return new IntOptionParser(String::valueOf);
     }
 }

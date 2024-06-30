ed7199d2c880f70460caf157bd83579281ac075e
Jiajun-Peng <pengjiajun566@gmail.com>
2024-06-30 12:46:55 +0800

    增加多个参数的测试用例，测试不通过

--- src/test/java/world/nobug/ArgsTest.java
+++ src/test/java/world/nobug/ArgsTest.java
@@ -57,6 +57,18 @@ public class ArgsTest {
 
     // multiple options:
     //  TODO: -l -p 8080 -d /usr/logs
+    @Test
+    public void should_parse_multi_options() {
+        MultiOptions options = Args.parse(MultiOptions.class, "-l", "-p", "8080", "-d", "/usr/logs");
+        assertTrue(options.logging());
+        assertEquals(8080, options.port());
+        assertEquals("/usr/logs", options.directory());
+    }
+
+    static record MultiOptions(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
+
+    }
+
     // sad path:
     //  TODO: -int -p | -p 8080 8081
     //  TODO: -string -d | -d /usr/logs /usr/vars
@@ -66,16 +78,6 @@ public class ArgsTest {
     //  TODO: -string : ""
 
 
-    // -l -p 8080 -d /usr/logs
-    @Test
-    @Disabled
-    public void should_example_1() {
-        Options options = Args.parse(Options.class, "-l", "-p", "8080", "-d", "/usr/logs");
-        assertTrue(options.logging());
-        assertEquals(8080, options.port());
-        assertEquals("/usr/logs", options.directory());
-    }
-
     // -g this is a list -d 1 2 -3 5
     @Test
     @Disabled
@@ -83,10 +85,6 @@ public class ArgsTest {
         ListOptions options = Args.parse(ListOptions.class, "-g", "this", "is", "a", "list", "-d", "1", "2", "-3", "5");
         assertArrayEquals(new String[] { "this", "is", "a", "list" }, options.group());
         assertArrayEquals(new int[] { 1, 2, -3, 5}, options.decimals());
-    }
-
-    static record Options(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
-
     }
 
     static record ListOptions(@Option("g") String[] group, @Option("d") int[] decimals) {

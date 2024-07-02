1a15a6bd94824cd1e20c374ca262bce044228463
Jiajun-Peng <pengjiajun566@gmail.com>
2024-07-02 22:26:40 +0800

    步骤1，增加可返回的属性

--- src/main/java/world/nobug/SingleValuedOptionParser.java
+++ src/main/java/world/nobug/SingleValuedOptionParser.java
@@ -5,15 +5,21 @@ import java.util.function.Function;
 
 class SingleValuedOptionParser<T> implements OptionParser<T> {
     Function<String, T> valueParser;
+    T defaultValue;
 
     public SingleValuedOptionParser(Function<String, T> valueParser) {
         this.valueParser = valueParser;
     }
 
+    public SingleValuedOptionParser(Function<String, T> valueParser, T defaultValue) {
+        this.valueParser = valueParser;
+        this.defaultValue = defaultValue;
+    }
+
     @Override
     public T parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
-        if (index == -1) return 0; // 这里我们希望返回0，但是不符合方法的定义，所以这里是一个错误的实现
+        if (index == -1) return defaultValue;
         if (arguments.size() == index + 1 ||
                 arguments.get(index + 1).startsWith("-")) throw new InsufficientArgumentsException(option.value());
         if (arguments.size() > index + 2 &&

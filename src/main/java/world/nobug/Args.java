d3c320a283c650e5fc375748ea7db3adb66422fb
Jiajun-Peng <pengjiajun566@gmail.com>
2024-07-02 13:43:51 +0800

    重构，inline IntOptionParser的工厂方法，使得所有使用IntOptionParser的地方都通过传入以函数引用的方式来使用

--- src/main/java/world/nobug/Args.java
+++ src/main/java/world/nobug/Args.java
@@ -27,7 +27,7 @@ public class Args {
 
     private static Map<Class<?>, OptionParser> PARSERS = Map.of(
             boolean.class, new BooleanOptionParser(),
-            int.class, IntOptionParser.createIntOptionParser(),
+            int.class, new IntOptionParser(Integer::parseInt),
             String.class, new IntOptionParser(String::valueOf));
 
 }

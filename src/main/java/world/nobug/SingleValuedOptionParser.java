fb73a2f305fe30c78cf862f2f53f2ce933bba4f9
Jiajun-Peng <pengjiajun566@gmail.com>
2024-07-02 22:25:25 +0800

    修改生产代码，不可行的实现

--- src/main/java/world/nobug/SingleValuedOptionParser.java
+++ src/main/java/world/nobug/SingleValuedOptionParser.java
@@ -13,6 +13,7 @@ class SingleValuedOptionParser<T> implements OptionParser<T> {
     @Override
     public T parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
+        if (index == -1) return 0; // 这里我们希望返回0，但是不符合方法的定义，所以这里是一个错误的实现
         if (arguments.size() == index + 1 ||
                 arguments.get(index + 1).startsWith("-")) throw new InsufficientArgumentsException(option.value());
         if (arguments.size() > index + 2 &&

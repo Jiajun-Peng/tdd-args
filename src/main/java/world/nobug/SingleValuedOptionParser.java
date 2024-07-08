96d5551c0008657ac7a7844e117f6ca1de3a89e3
Jiajun-Peng <pengjiajun566@gmail.com>
2024-07-08 13:01:12 +0800

    重构，对于一些不太清晰的代码，我们可能会通过添加代码注释的方式清晰描述一下，但是还是要尽量避免在代码里加注释，因为很多时候改了代码但不一定改注释，造成注释和功能不同步，除非是很正式的设计细节的文档。 有一种方式是通过提取方法，使用方法名代替注释。

--- src/main/java/world/nobug/SingleValuedOptionParser.java
+++ src/main/java/world/nobug/SingleValuedOptionParser.java
@@ -16,11 +16,23 @@ class SingleValuedOptionParser<T> implements OptionParser<T> {
     public T parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
         if (index == -1) return defaultValue;
-        if (arguments.size() == index + 1 ||
-                arguments.get(index + 1).startsWith("-")) throw new InsufficientArgumentsException(option.value());
-        if (arguments.size() > index + 2 &&
-                !arguments.get(index + 2).startsWith("-")) throw new TooManyArgumentsException(option.value());
+        if (isReachEndOfList(arguments, index) ||
+                isFollowedByOtherFlag(arguments, index)) throw new InsufficientArgumentsException(option.value());
+        if (secondArgumentIsNotFlag(arguments, index)) throw new TooManyArgumentsException(option.value());
         return valueParser.apply(arguments.get(index + 1));
+    }
+
+    private static boolean secondArgumentIsNotFlag(List<String> arguments, int index) {
+        return arguments.size() > index + 2 &&
+                !arguments.get(index + 2).startsWith("-");
+    }
+
+    private static boolean isFollowedByOtherFlag(List<String> arguments, int index) {
+        return arguments.get(index + 1).startsWith("-");
+    }
+
+    private static boolean isReachEndOfList(List<String> arguments, int index) {
+        return arguments.size() == index + 1;
     }
 
 }
